375aa233031c3615d89dd9f1693931cfebd1ae55
sebastian_idzkowski <sebastian.idzkowski@connectis.pl>
2017-09-21 14:55:09 +0200

    Added ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,24 @@
 import {Recipe} from './recipe.model';
+import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
+  public recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Schnizel',
+      'This is simply a test',
+      'http://m.ocdn.eu/_m/828f8b3f1410e2e8cd1b8ae08558576a,51,1,0-20-1000-627-0.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe('Hamburger',
+      'This is simply a test',
+      'https://static.comicvine.com/uploads/scale_small/2/28140/1561271-hamburger.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2)
+      ])
   ];
 
   getRecipes() {
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Ingredient } from '../shared/ingredient.model';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppinglistService} from './shoppinglist.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  ingredients: Ingredient[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private shoppingListService: ShoppinglistService) {
   }
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientChange.subscribe(
+        (ingredients: Ingredient[]) => {
+          this.ingredients = ingredients;
+        }
+      );
   }
 }
